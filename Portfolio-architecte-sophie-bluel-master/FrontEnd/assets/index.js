2948e73e12b81baeca2202ac8a2ef164feaee3b9
jeremy <126868845+Marquesjeremy@users.noreply.github.com>
2023-05-30 09:35:28 +0200

    Add files via upload

---
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/index.js
@@ -0,0 +1,75 @@
+const filtres = document.querySelector(".filtres");
+const gallery = document.querySelector(".gallery");
+const allProject = document.querySelector("#all");
+
+// Balises pour les images et le titre
+const createFigureElement = (project) => {
+  const figure = document.createElement("figure");
+  const img = document.createElement("img");
+  const figCaption = document.createElement("figcaption");
+  gallery.append(figure);
+  figure.append(img, figCaption);
+  img.src = project.imageUrl;
+  figCaption.textContent = project.title;
+};
+
+// filtres
+const createFiltreElement = (arrayProject) => {
+  const allCategory = [];
+
+  arrayProject.map((category) => {
+    if (!allCategory.includes(category.category.name)) {
+      allCategory.push(category.category.name);
+    }
+  });
+
+  allCategory.map((displayFiltre) => {
+    let filtre = document.createElement("li");
+    filtres.append(filtre);
+    filtre.classList.add("filtre");
+    filtre.textContent = displayFiltre;
+
+    filtre.addEventListener("click", () => {
+      let sort = sortCategory(arrayProject, filtre.textContent);
+      gallery.innerHTML = "";
+      displayHomeElement(sort);
+    });
+  });
+};
+// Tri les catégories
+const sortCategory = (arrayProject, categoryFiltre) => {
+  const sort = arrayProject.filter((category) => {
+    return category.category.name === categoryFiltre;
+  });
+  return sort;
+};
+
+// Affiche les éléments sur la page
+const displayHomeElement = (arrayProject) => {
+  const element = arrayProject.map((project) => {
+    return createFigureElement(project);
+  });
+  return element;
+};
+
+// les données du serveur
+const getData = async (url) => {
+  try {
+    const requete = await fetch(url, {
+      method: "GET",
+    });
+
+    const data = await requete.json();
+    displayHomeElement(data);
+    createFiltreElement(data);
+
+    allProject.addEventListener("click", () => {
+      gallery.innerHTML = "";
+      displayHomeElement(data);
+    });
+  } catch (e) {
+    alert(e);
+  }
+};
+
+getData("http://localhost:5678/api/works");
